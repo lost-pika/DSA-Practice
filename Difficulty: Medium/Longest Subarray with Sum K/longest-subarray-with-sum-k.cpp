8c4ea2aaa050aac9995589886c029400c755a836
Srishti Verma <136918651+sinceresrishti@users.noreply.github.com>
2025-02-10 18:04:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.cpp
+++ Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.cpp
@@ -6,32 +6,32 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int longestSubarray(vector<int>& arr, int k) {
-        unordered_map<long long, int> preSumMap;  
-        int maxLen = 0;
-        int sum = 0;
+   int longestSubarray(vector<int>& arr, int k) {
+    unordered_map<long long, int> preSumMap;
+    long long sum = 0;
+    int maxLen = 0;
 
-        for (int i = 0; i < arr.size(); i++) {
-            sum += arr[i];
+    for (int i = 0; i < arr.size(); i++) {
+        sum += arr[i]; 
 
-            if (sum == k) {
-                maxLen = max(maxLen, i + 1);
-            }
-
-            long long rem = sum - k;
-
-            if (preSumMap.find(rem) != preSumMap.end()) {
-                int len = i - preSumMap[rem];
-                maxLen = max(maxLen, len);
-            }
-
-            if (preSumMap.find(sum) == preSumMap.end()) {
-                preSumMap[sum] = i;
-            }
+        if (sum == k) {
+            maxLen = max(maxLen, i + 1); 
         }
 
-        return maxLen;
+        long long rem = sum - k;
+        if (preSumMap.find(rem) != preSumMap.end()) {
+            int len = i - preSumMap[rem];
+            maxLen = max(maxLen, len);
+        }
+
+        if (preSumMap.find(sum) == preSumMap.end()) {
+            preSumMap[sum] = i;
+        }
     }
+
+    return maxLen;
+}
+
 };
 
 //{ Driver Code Starts.
