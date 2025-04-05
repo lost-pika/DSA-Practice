c32b3823f6340594c5e180a4290f39aec259f471
Srishti Verma <136918651+sinceresrishti@users.noreply.github.com>
2025-04-06 00:11:31 +0530

    Time: 0 ms (100%), Space: 10.2 MB (68.64%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -4,12 +4,14 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
 
+        // transpose
         for(int i = 0; i < n-1; i++){
             for(int j = i+1; j < n; j++){
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
 
+        // reverse every row
         for(int i = 0; i<n; i++){
             reverse(matrix[i].begin(), matrix[i].end());
         }
